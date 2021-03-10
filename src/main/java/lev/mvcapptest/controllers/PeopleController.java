ddbc876905deb15b032e34bd19f59aab73eae5fb
Lev <afler777@gmail.com>
2021-03-10 14:59:53 +0400

    branch rename

---
+++ src/main/java/lev/mvcapptest/controllers/PeopleController.java
@@ -0,0 +1,17 @@
+package lev.mvcapptest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    @GetMapping()
+    public String index(){
+        return null;
+    }
+
+}
