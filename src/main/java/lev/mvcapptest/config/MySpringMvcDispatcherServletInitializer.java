063ccde12f172f1d19201a9a8882e024a857b2d1
Lev <afler777@gmail.com>
2021-03-10 14:54:55 +0400

    Initial commit

---
+++ src/main/java/lev/mvcapptest/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package lev.mvcapptest.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    //describes app properties for the server
+    //~web.xml
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
